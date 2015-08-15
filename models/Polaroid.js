a5e2c95eb77ebec90df17cabbecdf247b382793c
Alexander Scott <xander@cadcoder.com>
2015-08-15 14:15:57 +1000

    Polaroid Model

---
+++ models/Polaroid.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+    twid: String
+    , active: Boolean
+    , author: String
+    , avatar: String
+    , body: String
+    , date: Date
+    , screenname: String
+});
+
+schema.statics.reset = function(callback){
+    Polaroid.remove({},function(err){
+        console.log('Polaroid: Dropping collection');
+        callback(err);
+    });
+};
+
+schema.statics.recent = function (limit, callback) {
+
+    if (!limit || limit == 0){
+        limit = 20;
+    }
+    var tweets = [];
+
+    Polaroid.find({}, 'twid screenname avatar body date')
+        .sort({date: 'desc'}).limit(limit).exec(function (err, docs) {
+
+            if (!err) {
+                tweets = docs;
+            }
+            callback(tweets);
+        });
+};
+
+module.exports = Polaroid = mongoose.model('Polaroid', schema);
