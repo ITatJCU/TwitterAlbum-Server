e54fa398ae65771d462d40661ce5336334b9ca7f
Alexander Scott <xander@cadcoder.com>
2015-08-15 14:07:53 +1000

    Config Template

---
+++ config.tpl.js
@@ -0,0 +1,11 @@
+module.exports = {
+  twitter: {
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+  },
+  mongodb:{
+    uri:'mongodb://localhost/twitaroid'
+  }
+}
