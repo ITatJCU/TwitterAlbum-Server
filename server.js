f4d705589fd5be200be37007da83966edda3dd71
Alexander Scott <xander@cadcoder.com>
2015-08-15 14:29:02 +1000

    Stream Connection

--- server.js
+++ server.js
@@ -30,3 +30,20 @@ server.get('/', function (req, res, next) {
 server.listen(port, function () {
     console.log('Twitaroid-Server listening on port ' + port);
 });
+
+var ws = require("nodejs-websocket");
+
+var webSocketServer = ws.createServer(function (conn) {
+    conn.on("close", function (code, reason) {
+        console.log("WebSocketServer: Client Connection closed")
+    });
+
+    conn.on("error", function (err) {
+      console.log("WebSocketServer: Error;", err);
+    });
+}).listen(port + 1);
+
+
+twit.stream('statuses/filter', {track: 'twitaroid_dev'}, function (stream) {
+        streamHandler(stream, socketServer);
+});
